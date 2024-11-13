7345d2996c3b5c5efc29c9a6cdb102637ade68e1
Overchargers <overchargers.26886@gmail.com>
2024-11-12 22:02:57 -0500

    Add placeholder for PickArm code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/PickArm.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.components;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class PickArm {
+
+    public PickArm(HardwareMap hardwareMap)
+    {
+    }
+}
