1f60c24f07706db7d9bf9b221b3801a66ec44bf9
Overchargers <overchargers.26886@gmail.com>
2024-11-12 22:03:13 -0500

    Add placeholder for LiftArm code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/LiftArm.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.components;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class LiftArm {
+
+    public LiftArm(HardwareMap hardwareMap)
+    {
+    }
+}
