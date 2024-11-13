a8f025b5e6c47f21bdc3eda64825f3db6fcec541
Overchargers <overchargers.26886@gmail.com>
2024-11-13 08:31:21 -0500

    Add placeholder for Tele code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/IntoTheDeepTele.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Drawing;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@TeleOp(name = "IntoTheDeepTele", group = "Into The Deep")
+public class IntoTheDeepTele extends LinearOpMode {
+    final private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode()
+    {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        // TODO: Initialize here
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // Main loop
+        while (opModeIsActive())
+        {
+            // Drive by gamepad
+            drive.setDrivePowers(new PoseVelocity2d(
+                    new Vector2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x
+                    ),
+                    -gamepad1.right_stick_x
+            ));
+            drive.updatePoseEstimate();
+
+            // Display telemetry on the driver hub
+            telemetry.addData("x", drive.pose.position.x);
+            telemetry.addData("y", drive.pose.position.y);
+            telemetry.addData("heading (deg)", Math.toDegrees(drive.pose.heading.toDouble()));
+            telemetry.update();
+
+            // Display telemetry on the FTC dashboard
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.fieldOverlay().setStroke("#3F51B5");
+            Drawing.drawRobot(packet.fieldOverlay(), drive.pose);
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+        }
+
+    }
+}
