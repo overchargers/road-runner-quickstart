53752cad898a1b15e190c12f5b7eb43ffd69ed2b
Sanjay Ramachandran <sanjayramachandran@kishores-mbp.mynetworksettings.com>
2024-11-17 10:48:31 -0500

    Add constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public final class Constants {
+    public static final double backward_drive_power = 0.2;
+    public static final double high_basket = 0.5;
+    public static final double low_basket = 0.77;
+    public static final double drive_low_power = 0.2;
+    public static final double drive_high_power = 0.9;
+    public static final double lift_arm_swing_servo_tuck = 0.32;
+    public static final double pick_arm_swing_servo_tuck = 0.3;
+    public static final double pick_arm_swing_servo_backwards = 0.6;
+}
