02ef2d6987261c8ea47116bbe0be62a89ddefdf1
Overchargers <overchargers.26886@gmail.com>
2024-11-29 21:29:55 -0500

    Update Drivetrain code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Drivetrain.java
@@ -1,10 +1,19 @@
 package org.firstinspires.ftc.teamcode.components;
 
+
+import static java.lang.Thread.sleep;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 public class Drivetrain {
@@ -12,10 +21,12 @@ public class Drivetrain {
     Gamepad gamepad1;
     double drivePower;
     boolean direction_is_reverse;
+    private Servo batteringramservo;
 
     public Drivetrain(HardwareMap hardwareMap, Gamepad gp, MecanumDrive md){
         gamepad1 = gp;
         drive = md;
+        batteringramservo = hardwareMap.get(Servo.class, "batteringramservo");
     }
     public void drive_motor(){
         int reverse_turn;
@@ -51,4 +62,43 @@ public class Drivetrain {
     public void reverse_drive_direction() {
         direction_is_reverse = !direction_is_reverse;
     }
+    /**
+     * Describe this function...
+     */
+    public void battering_ram_in() {
+        batteringramservo.setPosition(Constants.battering_ram_is_in);
+    }
+
+    //
+    public class BatteringRamIn implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            battering_ram_in ();
+            return false;
+        }
+    }
+    public Action battering_ram_in_action()
+    {
+        return new BatteringRamIn();
+    }
+
+    /**
+     * Describe this function...
+     */
+    private void battering_ram_out() {
+        batteringramservo.setPosition(Constants.battering_ram_is_out);
+    }
+    //
+    public class BatteringRamOut implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            battering_ram_out();
+            return false;
+        }
+    }
+    public Action battering_ram_out_action()
+    {
+        return new BatteringRamOut();
+    }
+
 }
