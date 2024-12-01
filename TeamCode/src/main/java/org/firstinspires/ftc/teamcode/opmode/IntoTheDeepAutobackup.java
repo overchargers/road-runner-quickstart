c8caa8941a0aa1de522330752d85696e9cfa1c0c
Overchargers <overchargers.26886@gmail.com>
2024-12-01 10:30:38 -0500

    Update IntoTheDeepAutobackup code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/IntoTheDeepAutobackup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/IntoTheDeepAutobackup.java
@@ -7,6 +7,7 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
 @Autonomous(name = "IntoTheDeepAutoBackup", group = "Into The Deep")
+@Disabled
 public class IntoTheDeepAutobackup extends LinearOpMode {
 
     final private ElapsedTime runtime = new ElapsedTime();
