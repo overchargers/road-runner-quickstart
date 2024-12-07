60f946cfb59d5d693ad73f219e940ef75968596d
Overchargers <overchargers.26886@gmail.com>
2024-12-07 12:05:29 -0500

    Update  TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -9,9 +9,13 @@ public final class Constants {
     public static final double drive_low_power = 0.2;
     public static final double drive_high_power = 0.9;
     public static final double lift_arm_swing_servo_tuck = 0.32;
-    public static final double pick_arm_swing_servo_tuck = 0.3;
-    public static final double pick_arm_swing_servo_backwards = 0.6;
+    public static final double pick_arm_swing_servo_tuck = 0.2;
+    public static final double pick_arm_swing_servo_backwards = 0.5;
     public static final double battering_ram_is_out = 0.37;
     public static final double battering_ram_is_in = 0.73;
+
+    // path profile parameters for sample pickup auto (in inches)
+    public static final double maxWheelVel = 80;
+    public static final double maxProfileAccel = 50;
 
 }
