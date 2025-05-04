c693a1db33d2236769c55ebd20a2af5e04df0cfc
Overchargers <overchargers.26886@gmail.com>
2025-05-04 06:14:48 -0400

    Constants for various game and robot parameters

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+public final class Constants {
+    public static final String SAMPLE_SCORING = "SAMPLE-SCORING";
+    public static final String SPECIMEN_SCORING = "SPECIMEN-SCORING";
+    public static final double backward_drive_power = 0.2;
+    public static final double high_basket = 0.55;
+    public static final double low_basket = 0.79;
+    public static final double drive_low_power = 0.2;
+    public static final double drive_high_power = 0.9;
+    public static final double lift_arm_swing_servo_tuck = 0.3;
+    public static final double pick_arm_swing_servo_tuck = 0.2;
+    public static final double pick_arm_swing_servo_backwards = 0.5;
+    public static final double battering_ram_is_out = 0.37;
+    public static final double battering_ram_is_in = 0.73;
+    public static final double clip_close = 0.5;
+    public static final double clip_open = 0.23;
+    // path profile parameters for sample pickup auto (in inches)
+    public static final double maxWheelVel = 80;
+    public static final double maxProfileAccel = 50;
+}
